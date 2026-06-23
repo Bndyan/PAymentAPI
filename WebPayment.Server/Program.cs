75a613c1693a6f752d8dfac5fda139682d5cedf3
Karwan Karasool Hasan Bndyan <kbndyan@neusta.de>
2026-06-24 00:23:20 +0200

    Finishing CRUD Payment API

--- WebPayment.Server/Program.cs
+++ WebPayment.Server/Program.cs
@@ -49,6 +49,10 @@ if (app.Environment.IsDevelopment())
     );
 }
 
+app.UseCors(options => 
+    options.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 
 app.UseAuthorization();
 
